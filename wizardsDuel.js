ee364565c461531a643d6a0736422220d00e6e67
Blake Williams <williamsm3@dewv.edu>
2018-09-05 16:40:40 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,4 +1,4 @@
-import {game, Sprite} from "../sgc/sgc.js";
+import {game, Sprite} from "./sgc/sgc.js";
 
 game.setBackground("floor.png");
 
@@ -6,12 +6,26 @@ class PlayerWizard extends Sprite {
     constructor() {
         super();
         this.name = "Marcus the Wizard";
-        this.setImage = ("marcusSheet.png");
+        this.setImage("marcusSheet.png");
         this.width = 48;
         this.height = 48;
         this.x = this.width;
         this.y = this.y;
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 6, 8);
+        this.speedWhenWalking = 100;
+        
+    }
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
     }
 }
+let marcus = new PlayerWizard;
 
-let marcus = new PlayerWizard();
